daa31ba1fbe27b5a5c29da493b68e0c4ba1d9d3d
srabonAich <srabon-2022115890@cs.du.ac.bd>
2025-06-03 00:50:44 +0600

    0/1 knapsack updated

--- src/dp/Knapsack0_1.java
+++ src/dp/Knapsack0_1.java
@@ -9,7 +9,7 @@ wt[]=2,5,1,3,4
 W(total allowed weight) = 7
 ans = max profit
 
-recursive solution
+===========__recursive solution__=========
 knapsack(val[],wt[],W,idx i)
 for each product I have two choices: include , exclude
         if(wt <= W) // valid product
@@ -21,6 +21,53 @@ base case
     two things are changing W(capacity) and index i
     if W==0 -> ans = 0
     if i==0 -> ans = 0 //recursion will go from n to 0
+
+
+=========____solve using Memoization____===========
+in my knapsack function, there are 4 parameters. Among them only 2 are variable
+So I have to create a 2D array to memoize.
+        int[][] dp = new int[n+1][W+1]
+why n+1 and W+1? not n and W?
+cz the Base Case checks if n == 0 and W == 0, in memoization,
+the base case also gets included as Initialization step
+
+****_What does the 2D matrix signifies?_****
+every cell (i,j) bole je oi W er bag size jnno shob possible item theke
+maximum koto profit ber hobe.
+
+-> initialize every cell with -1
+-> final answer will be at cell (n+1,W+1)
+
+
+
+=======____solve using Tabulation____======
+->make a 2D matrix
+    int[][] dp = new int[n+1][W+1] ---- same logic
+->meaning of the table ---- same
+    every i,j cell tells the max profit of
+    'i' items having 'j' size left in the bag
+->init the table - base case
+    0th row and 0th column initialized by 0
+->fill (bottom to up)
+    for(int i=1 to n+1) -- dp array is from 0 to n+1
+                        -- 0 is initialized
+      for(int j=1 to j+1)
+           if(wt<=j) // valid items
+              case1 include the item
+                val[i-1] + dp[i-1][j-wt]
+                 // why i-1? dp er ith index
+                    val er i-1 th index er shoman
+                 // include korar por ja wt baki thakbe
+                    tar jnno max profit koto ta oi item
+                    er value er shathe add koro
+              case2 exclude the item
+                dp[i-1][j]
+                 // exclude korle wt change hobe na
+
+            Max of case1 and case2
+            dp[i][j]=max(val[i-1]+dp[i-1][j-wt],dp[i-1][j])
+
+            else  // invalid
 
 
  */
@@ -37,10 +84,60 @@ public class Knapsack0_1 {
             return knapsack(val,wt,W,n-1);
     }
 
+    // memoization
+    public static int knapsack(int[]val,int[]wt,int W,int n,int[][] dp){
+        if(W==0 || n==0) return 0;
+
+        if(dp[n][W] != -1) return dp[n][W];
+
+        if(wt[n-1] <= W) {
+            //include
+            int ans1=val[n-1]+knapsack(val,wt,W-wt[n-1],n-1,dp);
+            //exclude
+            int ans2=knapsack(val,wt,W,n-1,dp);
+            dp[n][W] = Math.max(ans1,ans2);
+            return dp[n][W];
+        }else {// not valid
+            dp[n][W] = knapsack(val,wt,W,n-1,dp);
+            return dp[n][W];
+        }
+    }
+
+    // tabulation
+    public static int knapsackTab(int[]val, int[]wt, int W){
+        int n = val.length;
+        int [][] dp = new int[n+1][W+1];
+        for(int i=0; i<dp.length; i++){
+            dp[i][0] = 0;
+        }
+        for(int j=0; j<dp[0].length; j++){
+            dp[0][j] = 0;
+        }
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<W+1; j++){
+                int v = val[i-1];
+                int w = wt[i-1];
+                if(w<=j){
+                    dp[i][j] = Math.max(v+dp[i-1][j-w],dp[i-1][j]);
+                }else{
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return dp[n][W];
+    }
+
     public static void main(String[] args) {
         int[] val = {15,14,10,45,30};
         int[] wt = {2,5,1,3,4};
         int W = 7;
-        System.out.println(knapsack(val,wt,W,val.length));
+        int[][] dp = new int[val.length+1][W+1];
+        for(int i=0; i< dp.length; i++){
+            for(int j=0; j<dp[0].length;j++){
+                dp[i][j] = -1;
+            }
+        }
+        //System.out.println(knapsack(val,wt,W,val.length,dp));
+        System.out.println(knapsackTab(val,wt,W));
     }
 }
