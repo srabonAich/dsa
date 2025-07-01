676f1eeca38e10809a1de7804897b028683bf9b8
srabonAich <srabon-2022115890@cs.du.ac.bd>
2025-07-01 12:04:21 +0600

    updated knapsack by abdul bari

--- src/dp/Knapsack0_1.java
+++ src/dp/Knapsack0_1.java
@@ -107,18 +107,18 @@ public class Knapsack0_1 {
     public static int knapsackTab(int[]val, int[]wt, int W){
         int n = val.length;
         int [][] dp = new int[n+1][W+1];
-        for(int i=0; i<dp.length; i++){
-            dp[i][0] = 0;
-        }
-        for(int j=0; j<dp[0].length; j++){
-            dp[0][j] = 0;
-        }
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<W+1; j++){
-                int v = val[i-1];
-                int w = wt[i-1];
-                if(w<=j){
-                    dp[i][j] = Math.max(v+dp[i-1][j-w],dp[i-1][j]);
+//        for(int i=0; i<dp.length; i++){
+//            dp[i][0] = 0;
+//        }
+//        for(int j=0; j<dp[0].length; j++){
+//            dp[0][j] = 0;
+//        }
+        for(int i=0; i<=n; i++){
+            for(int j=0; j<=W; j++){
+
+                if(i == 0 || j==0) dp[i][j] = 0;
+                else if(wt[i-1] <= j){
+                    dp[i][j] = Math.max(val[i-1] + dp[i-1][j-wt[i-1]] , dp[i-1][j]);
                 }else{
                     dp[i][j] = dp[i-1][j];
                 }
@@ -128,15 +128,15 @@ public class Knapsack0_1 {
     }
 
     public static void main(String[] args) {
-        int[] val = {15,14,10,45,30};
-        int[] wt = {2,5,1,3,4};
+        int[] val = {0,15,14,10,45,30};
+        int[] wt = {0,2,5,1,3,4};
         int W = 7;
-        int[][] dp = new int[val.length+1][W+1];
-        for(int i=0; i< dp.length; i++){
-            for(int j=0; j<dp[0].length;j++){
-                dp[i][j] = -1;
-            }
-        }
+//        int[][] dp = new int[val.length+1][W+1];
+//        for(int i=0; i< dp.length; i++){
+//            for(int j=0; j<dp[0].length;j++){
+//                dp[i][j] = -1;
+//            }
+//        }
         //System.out.println(knapsack(val,wt,W,val.length,dp));
         System.out.println(knapsackTab(val,wt,W));
     }
